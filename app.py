0eccab8db921715cd450d7ad3e33ee80bffd3000
INFRAESTRUCTURA 03 UIT <INFRAESTRUCTURA03@MINEDU.GOB.PE>
2025-08-01 15:37:54 -0500

    Desplegar en render

---
+++ app.py
@@ -0,0 +1,18 @@
+
+from flask import Flask
+from flask_cors import CORS
+from flask import Flask, render_template
+
+app = Flask(__name__,static_folder="static",template_folder="templates")
+CORS(app)
+
+@app.route("/checkAgent.html")
+def equipo_html():
+    return render_template("checkAgent.html")
+
+@app.route("/")
+def home():
+    return render_template("checkAgent.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
